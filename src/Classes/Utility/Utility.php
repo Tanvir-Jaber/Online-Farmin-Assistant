d6b54bdd1f73dc3cef0b86eca565997fe79b4e0e
Tanvir-Jaber <86012209+Tanvir-Jaber@users.noreply.github.com>
2021-06-20 21:54:02 +0600

    Add files via upload

---
+++ src/Classes/Utility/Utility.php
@@ -0,0 +1,17 @@
+
+namespace App\Utility;
+
+
+class Utility
+{
+ static function varDump($array){
+     echo "<pre>";
+     var_dump($array);
+     echo "</pre>";
+ }
+    static function redirect($url){
+
+
+        header("Location: $url" );
+    }
+}
